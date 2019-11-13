d8a18e7d34cfac548b94f0046087bf86177167c0
Lunnatica <paulalopezantelo@gmail.com>
2019-11-13 21:33:39 +0000

    Adding some changes to the compareTheTriplets exercise

--- JavaScript/compareTheTriplets.js
+++ JavaScript/compareTheTriplets.js
@@ -3,23 +3,17 @@
 // points on a scale from to for three categories: problem clarity, originality, and difficulty.
 // We define the rating for Alice's challenge to be the triplet , and the rating for Bob's
 // challenge to be the triplet .
-// Your task is to find their comparison points by comparing with , with , and with .
-// If , then Alice is awarded point.
-// If , then Bob is awarded point.
-// If , then neither person receives a point.
+// Your task is to find their comparison points 
 // Comparison points is the total points a person earned.
-// Given and , determine their respective comparison points.
-// For example, and . For elements , Bob is awarded a point because .
-// For the equal elements and , no points are earned. Finally, for elements , so Alice
-// receives a point. Your return array would be with Alice's score first and Bob's second.
+// Determine their respective comparison points.
+// For the equal elements, no points are earned. 
+// Your return array would be with Alice's score first and Bob's second.
 // Function Description
-// Complete the function compareTriplets in the editor below. It must return an array of two integers, the
+// Complete the function compareTriplets below. It must return an array of two integers, the
 // first being Alice's score and the second being Bob's.
 // compareTriplets has the following parameter(s):
 // a: an array of integers representing Alice's challenge rating
 // b: an array of integers representing Bob's challenge rating
-// Input Format
-// The first line contains space-separated integers, , 
 
 'use strict';
 
