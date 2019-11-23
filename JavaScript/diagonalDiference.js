66e8a0fc30197384e42981e625570b54d7508510
Lunnatica <paulalopezantelo@gmail.com>
2019-11-23 17:59:30 +0000

    Added diagonal difference problem

---
+++ JavaScript/diagonalDiference.js
@@ -0,0 +1,63 @@
+// Diagonal difference - https://www.hackerrank.com/challenges/diagonal-difference/problem
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+    let firstDiagonal = 0;
+    let secDiagonal = 0;
+    for (let i = 0; i < arr.length; i++) { 
+            firstDiagonal += arr[i][i]
+            secDiagonal += arr[i][arr.length - 1 - i]    
+            console.log(secDiagonal)  
+    } 
+    return Math.abs(firstDiagonal - secDiagonal);
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
