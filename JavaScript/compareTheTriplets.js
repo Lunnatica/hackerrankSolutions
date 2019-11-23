e2eb7e441115d0c76790dda03b85091ebceb4997
Lunnatica <paulalopezantelo@gmail.com>
2019-11-23 18:47:42 +0000

    Added compare the triplets problem

--- JavaScript/compareTheTriplets.js
+++ JavaScript/compareTheTriplets.js
@@ -13,7 +13,7 @@
 // first being Alice's score and the second being Bob's.
 // compareTriplets has the following parameter(s):
 // a: an array of integers representing Alice's challenge rating
-// b: an array of integers representing Bob's challenge rating
+// b: an array of integers representing Bob's challenge rating 
 
 'use strict'; 
 
